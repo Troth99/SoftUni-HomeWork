17b1702be0e4624319d6258ab2919bca301ddb03
Troth99 <trothlol123456@gmail.com>
2025-02-05 05:24:29 +0200

    Update solution.js

--- JS Advanced Modul/DOM MANIPULATION Exercises/06. Furniture/solution.js
+++ JS Advanced Modul/DOM MANIPULATION Exercises/06. Furniture/solution.js
@@ -18,7 +18,7 @@ function solve() {
       const itemsObj = {
         image: item.img,
         name: item.name,
-        price:item.price,
+        price: item.price,
         decFac: item.decFactor,
 
       }
@@ -29,15 +29,15 @@ function solve() {
       let trEl = document.createElement('tr')
       const trApend = tableEl.appendChild(trEl)
       //create tables for each item
-      
+
       Object.values(itemsObj).forEach((value, index) => {
         const tdCr = document.createElement('td');
         if (index === 0) {
           const imgEl = document.createElement('img')
           imgEl.src = value
           tdCr.appendChild(imgEl);
-          
-        }else {
+
+        } else {
           const pEl = document.createElement('p');
           pEl.textContent = value;
           tdCr.appendChild(pEl)
@@ -45,7 +45,7 @@ function solve() {
 
         trApend.appendChild(tdCr)
       })
-   
+
       //createCheckbox
       const tdCheck = document.createElement('td');
       const checkBox = document.createElement('input');
@@ -55,35 +55,35 @@ function solve() {
 
     });
   }
-const btnBuy = document.querySelectorAll('button')[1]
+  const btnBuy = document.querySelectorAll('button')[1]
 
-btnBuy.addEventListener('click', buying);
+  btnBuy.addEventListener('click', buying);
 
-function buying(){
-  const checkBoxes = document.querySelectorAll('input[type=checkbox]')
-  let totalPrice = 0
-  let boughtFurniture = []
-  let totalDecFac = 0
-  let checked = 0
-  checkBoxes.forEach(check => {
-   if (check.checked){
-    const row = check.closest('tr');
-    const nameItem = row.children[1].textContent
-    boughtFurniture.push(nameItem)
-    const price = Number(row.children[2].textContent)
-    const decFac = Number(row.children[3].textContent)
-    totalDecFac += decFac
-    totalPrice += price
-    checked ++
-   }
-  })
-  let avgDecFac= totalDecFac / checked
-  const textAreaOutput = document.querySelectorAll('textarea')[1];
-  textAreaOutput.value += `Bought furniture: ${boughtFurniture.join(', ')}\n`
-  textAreaOutput.value += `Total price: ${totalPrice.toFixed(2)}\n`
-  textAreaOutput.value += `Average decoration factor: ${avgDecFac}`
+  function buying() {
+    const checkBoxes = document.querySelectorAll('input[type=checkbox]')
+    let totalPrice = 0
+    let boughtFurniture = []
+    let totalDecFac = 0
+    let checked = 0
+    checkBoxes.forEach(check => {
+      if (check.checked) {
+        const row = check.closest('tr');
+        const nameItem = row.children[1].textContent
+        boughtFurniture.push(nameItem)
+        const price = Number(row.children[2].textContent)
+        const decFac = Number(row.children[3].textContent)
+        totalDecFac += decFac
+        totalPrice += price
+        checked++
+      }
+    })
+    let avgDecFac = totalDecFac / checked
+    const textAreaOutput = document.querySelectorAll('textarea')[1];
+    textAreaOutput.value += `Bought furniture: ${boughtFurniture.join(', ')}\n`
+    textAreaOutput.value += `Total price: ${totalPrice.toFixed(2)}\n`
+    textAreaOutput.value += `Average decoration factor: ${avgDecFac}`
 
-}
+  }
 
 }
 
