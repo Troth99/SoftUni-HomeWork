be91a5ab93c7d3396a160404e7f48bfddc331de1
Troth99 <trothlol123456@gmail.com>
2025-02-27 09:00:53 +0200

    Доне

--- JS applications/Asynchronus Programing - exercies/02.Bus-Schedule/app.js
+++ JS applications/Asynchronus Programing - exercies/02.Bus-Schedule/app.js
@@ -1,11 +1,35 @@
 function solve() {
+    const departBtn = document.getElementById('depart');
+    const ariveBnt = document.getElementById('arrive')
+    const showEl = document.querySelector('#info span')
+    const url = `http://localhost:3030/jsonstore/bus/schedule/`
 
-    function depart() {
-        console.log('Depart TODO...');
+    const stopId = {
+        name: '',
+        next: 'depot'
     }
+    function depart() {
+        fetch(url + stopId.next)
+            .then((response) => {
+                return response.json()
+            })
+            .then((result) => {
+                stopId.name = result.name
+                stopId.next = result.next
+                showEl.textContent = `Next stop ${stopId.name}`
+                departBtn.disabled = true
+                ariveBnt.disabled = false
 
+            }).catch((err) => {
+                showEl.textContent = 'Error'
+                departBtn.disabled = true;
+                ariveBnt.disabled = true
+            })
+    }
     function arrive() {
-        console.log('Arrive TODO...');
+        ariveBnt.disabled = true;
+        showEl.textContent = `Arriving at ${stopId.name}`
+        departBtn.disabled = false
     }
 
     return {
--- JS applications/Asynchronus Programing - exercies/06.Blog/app.js
+++ JS applications/Asynchronus Programing - exercies/06.Blog/app.js
@@ -1,5 +1,68 @@
 function attachEvents() {
-    console.log('TODO...');
+
+    const postsUrl = 'http://localhost:3030/jsonstore/blog/posts';
+    const commentsUrl = 'http://localhost:3030/jsonstore/blog/comments'
+
+    const btnLoadPosts = document.getElementById('btnLoadPosts');
+    const btnViewPosts = document.getElementById('btnViewPost')
+    
+    const postTitleEl = document.getElementById('post-title');
+    const postsEl = document.getElementById('posts')
+    const postBodyEl = document.getElementById('post-body');
+    const commentsEl = document.getElementById('post-comments')
+
+    btnLoadPosts.addEventListener('click', postRequests)
+    btnViewPosts.addEventListener('click', gettingData)
+
+  
+    async function postRequests(e) {
+        try {
+            const response = await fetch(postsUrl);
+            const data = await response.json()
+            let entries = Object.entries(data)
+
+            for (const element of entries) {
+                const optionEL = document.createElement('option')
+                optionEL.value = element[0];
+                optionEL.textContent = element[1].title
+                postsEl.appendChild(optionEL)
+            }
+        } catch (err) {
+            throw new Error('Error')
+        }
+    }
+   
+    async function gettingData() {
+        const selectedPostId = postsEl.value;
+    
+        try {
+            const responseFromPost = await fetch(postsUrl);
+            const dataFromPost = await responseFromPost.json();
+    
+            const responseFromComments = await fetch(commentsUrl);
+            const dataFromComments = await responseFromComments.json();
+    
+       
+            const selectedPost = dataFromPost[selectedPostId];
+
+            postTitleEl.textContent = selectedPost.title;
+            postBodyEl.textContent = selectedPost.body;
+            commentsEl.innerHTML = '';
+    
+            const comments = Object.values(dataFromComments).filter(comment => comment.postId === selectedPostId);
+    
+                comments.forEach(comment => {
+                    const li = document.createElement('li');
+                    li.textContent = comment.text;
+                    commentsEl.appendChild(li);
+                });
+           
+    
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    
 }
 
 attachEvents();
