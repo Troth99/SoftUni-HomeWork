8aa98b3bfc36b6684bfe0aa0b2a83ebd0d052d13
Troth99 <trothlol123456@gmail.com>
2025-03-01 05:54:30 +0200

    Async programing all tasks done

--- JS applications/Asynchronus Programing - exercies/04.Locked-Profile/app.js
+++ JS applications/Asynchronus Programing - exercies/04.Locked-Profile/app.js
@@ -1,3 +1,75 @@
 function lockedProfile() {
-    console.log('TODO...')
+
+    const getUrl = 'http://localhost:3030/jsonstore/advanced/profiles'
+
+    const mainEl = document.getElementById('main');
+    mainEl.innerHTML = ''
+    fetch(getUrl)
+        .then((response) => {
+            return response.json()
+        }).then((result) => {
+            const entries = Object.entries(result)
+
+            entries.forEach((user) => {
+                const name = user[1].username
+                const email = user[1].email;
+                const age = user[1].age
+                const createDivEl = document.createElement('div')
+                createDivEl.setAttribute('class', 'profile')
+                createDivEl.innerHTML = `
+				<img src="./iconProfile2.png" class="userIcon" />
+				<label>Lock</label>
+				<input type="radio" name="user1Locked" value="lock" checked>
+				<label>Unlock</label>
+				<input type="radio" name="user1Locked" value="unlock"><br>
+				<hr>
+				<label>Username</label>
+				<input type="text" name="user1Username" value="${name}" disabled readonly />
+				<div class="user1Username">
+					<hr>
+					<label>Email:</label>
+					<input type="email" name="user1Email" value="${email}" disabled readonly />
+					<label>Age:</label>
+					<input type="number" name="user1Age" value="${age}" disabled readonly />
+				</div>
+				<button>Show more</button>
+            `
+                mainEl.appendChild(createDivEl)
+            })
+
+            const user1UsernameEL = document.querySelectorAll('.user1Username');
+   
+            user1UsernameEL.forEach((userInfo) => {
+                userInfo.style.display = 'none'
+            })
+
+
+            const showMoreBtn = document.querySelectorAll('#main .profile button')
+
+            for (const button of showMoreBtn) {
+                button.addEventListener('click', (e) => showMore(e))
+            }
+
+            function showMore(e) {
+        
+                const button = e.currentTarget;
+                const profile = button.parentElement;
+               
+                const userDetails = profile.querySelector('.user1Username');
+                const isLocked = profile.querySelector('input[value="lock"]')
+
+                if(isLocked.checked){
+                   console.log('Try again');
+                   return
+             }else {
+                 userDetails.style.display = userDetails.style.display === 'none' ? 'block': 'none'
+                 button.textContent = button.textContent === 'Show more' ? 'Hide it': 'Show more'
+             }
+            }
+
+        }).catch((err) => [
+            console.log('Error')
+        ])
+
+
 }
--- JS applications/Asynchronus Programing - exercies/05. Accordion/accordion.js
+++ JS applications/Asynchronus Programing - exercies/05. Accordion/accordion.js
@@ -1,3 +1,63 @@
 function solution() {
-    //TODO .....
+  const url = 'http://localhost:3030/jsonstore/advanced/articles/list';
+  const detailsUrl = 'http://localhost:3030/jsonstore/advanced/articles/details';
+  const sectionEL = document.getElementById('main');
+
+  async function request() {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      for (const element of data) {
+        const createdDivEl = document.createElement('div');
+        createdDivEl.classList.add('accordion');
+
+        createdDivEl.innerHTML = `
+          <div class="head">
+            <span>${element.title}</span>
+            <button class="button" id="${element._id}">More</button>
+          </div>
+          <div class="extra" style="display: none;"></div>
+        `;
+
+        sectionEL.appendChild(createdDivEl);
+      }
+
+      const showMore = document.querySelectorAll('.button');
+      for (const element of showMore) {
+        element.addEventListener('click', extraDetails);
+      }
+
+    } catch (error) {
+      console.error('Error');
+    }
+  }
+
+  async function extraDetails(e) {
+    try {
+      const button = e.currentTarget;
+      let currentId = button.id;
+      const extra = button.parentElement.parentElement.querySelector('.extra');
+
+      const responseDetails = await fetch(`${detailsUrl}/${currentId}`);
+      const dataDetails = await responseDetails.json();
+      const content = dataDetails.content;
+
+      if (button.textContent.toLowerCase() === 'more') {
+        extra.innerHTML = `<p>${content}</p>`;
+        extra.style.display = 'block';
+        button.textContent = 'less';
+      } else {
+        extra.style.display = 'none';
+        button.textContent = 'more';
+      }
+
+    } catch (error) {
+      console.error('Error');
+    }
+  }
+
+  request();
 }
+
+solution();
